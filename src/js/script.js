bd9172ee81517ee11b118a50454014a8a3df3188
Lucas Massoni <lucas.masson0307@gmail.com>
2025-05-31 10:59:05 -0300

    slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,19 @@ const mobileNavBar = new MobileNavBar(
     ".nav-list li"
 );
 mobileNavBar.init();
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
